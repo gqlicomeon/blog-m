30558e9a43522f0c0e18ac1792e750fb4e0572e5
liguanqing <1244341574@qq.com>
2020-05-27 19:50:03 +0800

    click

--- src/components/TouchEffect/index.js
+++ src/components/TouchEffect/index.js
@@ -14,9 +14,9 @@ function TouchEffect(){
     const handler = (e)=>{
         let tagName = e.target.tagName.toLowerCase();
         let parentTagName = e.target.parentNode.tagName.toLowerCase();
-        if(tagName === "a" || parentTagName === "a"){//防止点击穿透导致a链接无法点击
-            return;
-        }
+        // if(tagName === "a" || parentTagName === "a"){//防止点击穿透导致a链接无法点击
+        //     return;
+        // }
         let timestamp = new Date().getTime();
         let {clientX,clientY} = e.touches[0];
         let randomColor = `color${Math.round(Math.random()*14+1)}`;
@@ -24,23 +24,25 @@ function TouchEffect(){
         let animationName = `float_top_${Math.round(Math.random()*9+1)}`;
         let effectMap = ["fa-star-o","fa-snowflake-o","fa-sun-o","fa-smile-o","fa-paper-plane-o","fa-heart-o","fa-hand-peace-o","fa-gift","fa-sign-language","fa-thumbs-o-up"];
         let randomEffect = effectMap[Math.round(Math.random()*9)];
-        setTouchesArr(state=>{
-            let style = {
-                 position:"fixed",
-                 left:`${clientX}px`,
-                 top:`${clientY}px`,
-                 marginLeft:"-0.5em",
-                 marginTop:"-0.5em",
-                 transform:"translateZ(0)",
-                 animation: `${animationName} ease-in-out 800ms both`
-            }
-            state.push({
-                timestamp,
-                style,
-                className:`fa ${randomEffect} ${randomColor} ${randomFont}`
-            });
-            return state.slice();
-        })
+        setTimeout(()=>{
+            setTouchesArr(state=>{
+                let style = {
+                     position:"fixed",
+                     left:`${clientX}px`,
+                     top:`${clientY}px`,
+                     marginLeft:"-0.5em",
+                     marginTop:"-0.5em",
+                     transform:"translateZ(0)",
+                     animation: `${animationName} ease-in-out 800ms both`
+                }
+                state.push({
+                    timestamp,
+                    style,
+                    className:`fa ${randomEffect} ${randomColor} ${randomFont}`
+                });
+                return state.slice();
+            })
+        },300)
     }
 
    
